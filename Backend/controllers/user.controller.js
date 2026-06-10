d889fce71fe7cae254ac5a71f71c4776890b94bb
UjjwalKandpal12 <ujjwalkandpal5@gmail.com>
2026-06-10 18:49:37 +0530

    Implemented update account route

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -222,3 +222,25 @@ export const getCurrentUser = asyncHandler(async (req, res) => {
     .status(200)
     .json(new ApiResponse(200, req.user, "User fetched successfully"));
 });
+
+export const updateAccountDetails = asyncHandler(async (req, res) => {
+  const { fullName, email } = req.body;
+  if (!fullName || !email) {
+    throw new ApiError(400, "Full name and email are required");
+  }
+  const user = await User.findByIdAndUpdate(
+    req.user?._id,
+    {
+      $set: {
+        fullName,
+        email,
+      },
+    },
+    {
+      new: true,
+    }
+  ).select("-password -refreshToken");
+  return res
+    .status(200)
+    .json(new ApiResponse(200, user, "Account details updated successfully"));
+});
