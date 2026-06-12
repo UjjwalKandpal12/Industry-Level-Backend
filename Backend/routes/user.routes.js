5397cd7b149392ebbf83e7d0419de370515691cb
UjjwalKandpal12 <ujjwalkandpal5@gmail.com>
2026-06-12 23:07:01 +0530

    add new routes for user controller

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -5,6 +5,13 @@ import { loginUser } from "../controllers/user.controller.js";
 import { verifyUser } from "../middlewares/auth.middleware.js";
 import { logoutUser } from "../controllers/user.controller.js";
 import { refreshAccessToken } from "../controllers/user.controller.js";
+import { getCurrentUser } from "../controllers/user.controller.js";
+import { updateAvatar } from "../controllers/user.controller.js";
+import { updateCoverImage } from "../controllers/user.controller.js";
+import { getUserChannelProfile } from "../controllers/user.controller.js";
+import { getWatchHistory } from "../controllers/user.controller.js";
+import { changeCurrentPassword } from "../controllers/user.controller.js";
+import { updateAccountDetails } from "../controllers/user.controller.js";
 const userRouter = Router();
 //routes declarations
 userRouter.post(
@@ -20,4 +27,16 @@ userRouter.post("/login", loginUser);
 //secured routes
 userRouter.post("/logout", verifyUser, logoutUser);
 userRouter.post("/refresh-access-token", refreshAccessToken);
+userRouter.post("/change-password", verifyUser, changeCurrentPassword);
+userRouter.get("/current-user", verifyUser, getCurrentUser);
+userRouter.patch("/update-account-details", verifyUser, updateAccountDetails);
+userRouter.patch("/avatar", verifyUser, upload.single("avatar"), updateAvatar);
+userRouter.patch(
+  "/cover-image",
+  verifyUser,
+  upload.single("coverImage"),
+  updateCoverImage
+);
+userRouter.get("/channel-profile/:username", verifyUser, getUserChannelProfile);
+userRouter.get("/watchHistory", verifyUser, getWatchHistory);
 export default userRouter;
